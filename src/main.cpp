f9ee28d9076aa0b4a2cc5d4b805575dd7058b5e9
davide di gloria <d.digloria@stamtech.com>
2024-10-29 23:20:22 +0100

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -5,37 +5,58 @@
 I2SStream i2s;
 BluetoothA2DPSink a2dp_sink(i2s);
 
-// for esp_a2d_connection_state_t see https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/bluetooth/esp_a2dp.html#_CPPv426esp_a2d_connection_state_t
-void connection_state_changed(esp_a2d_connection_state_t state, void *ptr)
-{
-    Serial.println(a2dp_sink.to_str(state));
-}
+#define PIN_BCK 26
+#define PIN_DATA 27
+#define PIN_WS 25
+#define PIN_RELAY 14
 
-// for esp_a2d_audio_state_t see https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/bluetooth/esp_a2dp.html#_CPPv421esp_a2d_audio_state_t
-void audio_state_changed(esp_a2d_audio_state_t state, void *ptr)
+int relay_status = 0;
+
+void avrc_metadata_callback(uint8_t id, const uint8_t *text)
 {
-    Serial.println(a2dp_sink.to_str(state));
+    Serial.printf("==> AVRC metadata rsp: attribute id 0x%x, %s\n", id, text);
+    if (id == ESP_AVRC_MD_ATTR_PLAYING_TIME)
+    {
+        uint32_t playtime = String((char *)text).toInt();
+        Serial.printf("==> Playing time is %d ms (%d seconds)\n", playtime, (int)round(playtime / 1000.0));
+    }
 }
 
 void setup()
 {
-    //rtc_wdt_protect_off();
-    //rtc_wdt_disable();
-
-    auto cfg = i2s.defaultConfig();
-    cfg.pin_bck = 26;
-    cfg.pin_data = 27;
-    cfg.pin_ws = 25;
+    pinMode(PIN_RELAY, OUTPUT);
+    audio_tools::I2SConfig cfg = i2s.defaultConfig();
+    cfg.pin_bck = PIN_BCK;
+    cfg.pin_data = PIN_DATA;
+    cfg.pin_ws = PIN_WS;
     i2s.begin(cfg);
-
-    a2dp_sink.set_on_connection_state_changed(connection_state_changed);
-    a2dp_sink.set_on_audio_state_changed(audio_state_changed);
+    a2dp_sink.set_avrc_metadata_attribute_mask(ESP_AVRC_MD_ATTR_TITLE | ESP_AVRC_MD_ATTR_ARTIST | ESP_AVRC_MD_ATTR_ALBUM | ESP_AVRC_MD_ATTR_PLAYING_TIME);
+    a2dp_sink.set_avrc_metadata_callback(avrc_metadata_callback);
     a2dp_sink.start("Cuccumella");
+
+    Serial.begin(115200);
+    Serial.println("Started");
 }
 
+int connected = 0, connected_old = 0;
+int cnt = 0;
 void loop()
 {
-    //rtc_wdt_feed();
-    //vTaskDelay(pdMS_TO_TICKS(100));
+    connected_old = connected;
+    connected = a2dp_sink.is_connected();
+
+    if (connected && !connected_old)
+    {
+        Serial.print(a2dp_sink.get_peer_name());
+        Serial.println(" connected");
+        digitalWrite(PIN_RELAY, 1);
+    }
+
+    if (!connected && connected_old)
+    {
+        Serial.println("Disconnected");
+        digitalWrite(PIN_RELAY, 0);
+    }
+
     delay(100);
 }
