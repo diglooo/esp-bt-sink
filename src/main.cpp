777a49395f91a53f6ef4735f632926ab1a59e411
Davide Di Gloria <62514786+diglooo@users.noreply.github.com>
2024-11-09 20:44:34 +0100

    fix state machine

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,21 @@
 #include "BluetoothA2DPSinkQueued.h"
 #include <Bonezegei_WS2812.h>
 
+// connections
 // On PCM5102A board, you must bridge SCK on PCB
 #define PIN_BCK 26  // PCM5102A BCK
 #define PIN_DATA 27 // PCM5102A DIN
 #define PIN_WS 25   // PCM5102A LCK
-#define PIN_RELAY 14
 #define PIN_PAIR_BUTTON 13
-#define PIN_LED_SERIAL 15
-#define BUTTON_PRESSED_THRESH 40 // touch limit
-#define NO_SOUND_TIMEOUT 30000
+#define PIN_RELAY 14
+#define PIN_LED_DATA 15
 
+// settings
+#define NO_SOUND_TIMEOUT_MS 60000
+#define BUTTON_PAIRING_WINDOW_MS 10000
+#define BLUETOOTH_DEVICE_NAME "Cuccumella"
+
+// state machine
 #define APP_STA_INIT 0
 #define APP_STA_DISCONNECTED 1
 #define APP_STA_PAIRING 2
@@ -21,18 +26,20 @@
 #define LED_QUANTITY 2
 
 // param WS2812 pin
-Bonezegei_WS2812 rgb_led(PIN_LED_SERIAL, LED_QUANTITY);
+Bonezegei_WS2812 rgb_led(PIN_LED_DATA, LED_QUANTITY);
 int relay_status = 0;
 int connected = 0, connected_old = 0;
 int cnt = 0;
 int pairing_millis = 0;
-long user_button_millis = 30000;
+long millisNow = 0;
+long user_button_millis = 0;
 long last_play_millis = 0;
 long last_blink_millis = 0;
 uint32_t blink_led_color = 0;
 uint32_t static_led_color = 0;
 int blink_var = 0;
 int user_ack = 0;
+int standby = 0;
 int APP_STATE = APP_STA_INIT;
 
 I2SStream i2s;
@@ -40,28 +47,19 @@ BluetoothA2DPSink a2dp_sink(i2s);
 
 void avrc_metadata_callback(uint8_t id, const uint8_t *text)
 {
-    Serial.printf("AVRC attribute id 0x%x = %s\n", id, text);
+    Serial.printf("AVRC data 0x%x = %s\n", id, text);
 }
 
-void audio_state_changed(esp_a2d_audio_state_t state, void *ptr)
+void audio_data_received_callback()
 {
-    if (state == ESP_A2D_AUDIO_STATE_STARTED)
-    {
-        last_play_millis = millis();
-    }
-}
-
-void setup()
-{
-    pinMode(PIN_PAIR_BUTTON, INPUT_PULLUP);
-    pinMode(PIN_RELAY, OUTPUT);
+    last_play_millis = millis();
 }
 
 void led_control()
 {
-    if (millis() - last_blink_millis > 500)
+    if (millisNow - last_blink_millis > 250)
     {
-        last_blink_millis = millis();
+        last_blink_millis = millisNow;
         if (blink_led_color)
         {
             if (blink_var)
@@ -83,44 +81,45 @@ void led_control()
         rgb_led.fill(0x00000000);
 }
 
+void setup()
+{
+}
+
 void loop()
 {
+    millisNow = millis();
     connected_old = connected;
     connected = a2dp_sink.is_connected();
-
-    if (!digitalRead(PIN_PAIR_BUTTON))
-    {
-        user_button_millis = millis();
-    }
-    user_ack = (millis() - user_button_millis < 15000);
 
     switch (APP_STATE)
     {
     case APP_STA_INIT:
     {
+        pinMode(PIN_RELAY, OUTPUT);
+        pinMode(PIN_PAIR_BUTTON, INPUT_PULLUP);
+        //Serial.println("APP_STA_INIT");
         Serial.begin(115200);
         rgb_led.begin();
         rgb_led.fill(0x000000);
-        Serial.println("APP_STA_INIT");
-        pinMode(PIN_RELAY, OUTPUT);
-        pinMode(PIN_PAIR_BUTTON, INPUT_PULLUP);
+
         audio_tools::I2SConfig cfg = i2s.defaultConfig();
         cfg.pin_bck = PIN_BCK;
         cfg.pin_data = PIN_DATA;
         cfg.pin_ws = PIN_WS;
         i2s.begin(cfg);
+
         a2dp_sink.set_avrc_metadata_attribute_mask(ESP_AVRC_MD_ATTR_TITLE | ESP_AVRC_MD_ATTR_ARTIST | ESP_AVRC_MD_ATTR_ALBUM);
         a2dp_sink.set_avrc_metadata_callback(avrc_metadata_callback);
-        a2dp_sink.set_on_audio_state_changed(audio_state_changed);
+        a2dp_sink.set_on_data_received(audio_data_received_callback);
         a2dp_sink.activate_pin_code(true);
-        a2dp_sink.start("Cuccumella");
+        a2dp_sink.start(BLUETOOTH_DEVICE_NAME);
         APP_STATE = APP_STA_DISCONNECTED;
         break;
     }
 
     case APP_STA_DISCONNECTED:
     {
-        Serial.println("APP_STA_DISCONNECTED");
+        //Serial.println("APP_STA_DISCONNECTED");
 
         digitalWrite(PIN_RELAY, 0);
         if (a2dp_sink.pin_code())
@@ -137,7 +136,7 @@ void loop()
 
     case APP_STA_PAIRING:
     {
-        Serial.println("APP_STA_PAIRING");
+        //Serial.println("APP_STA_PAIRING");
         if (user_ack)
         {
             a2dp_sink.confirm_pin_code();
@@ -145,7 +144,7 @@ void loop()
         }
         if (a2dp_sink.pin_code() == 0)
         {
-             user_button_millis = millis()+ 40000;
+            user_button_millis = 0;
             APP_STATE = APP_STA_DISCONNECTED;
         }
         break;
@@ -153,11 +152,12 @@ void loop()
 
     case APP_STA_CHECK_PAIRING:
     {
-        Serial.println("APP_STA_CHECK_PAIRING");
+        //Serial.println("APP_STA_CHECK_PAIRING");
         if (connected)
         {
             APP_STATE = APP_STA_CONNECTED;
             pairing_millis = 0;
+            last_play_millis = millis();
         }
         if (pairing_millis++ > 10 || a2dp_sink.pin_code() == 0)
         {
@@ -168,39 +168,37 @@ void loop()
 
     case APP_STA_PAIRING_FAILED:
     {
-        Serial.println("APP_STA_PAIRING_FAILED");
-         user_button_millis = millis()+ 40000;
+        //Serial.println("APP_STA_PAIRING_FAILED");
+        user_button_millis = 0;
         APP_STATE = APP_STA_DISCONNECTED;
         break;
     }
 
     case APP_STA_CONNECTED:
     {
-        Serial.println("APP_STA_CONNECTED");
-        digitalWrite(PIN_RELAY, 1);
-
-        /*    if (millis() - last_play_millis < NO_SOUND_TIMEOUT)
-            {
-                digitalWrite(PIN_RELAY, 1);
-            }
-            else
-            {
-                digitalWrite(PIN_RELAY, 0);
-            }
-    */
+        //Serial.println("APP_STA_CONNECTED");
+        digitalWrite(PIN_RELAY, !standby);
         if (!connected)
         {
             APP_STATE = APP_STA_DISCONNECTED;
-            user_button_millis = millis()+ 40000;
+            user_button_millis = 0;
         }
         break;
     }
     }
 
+    if (!digitalRead(PIN_PAIR_BUTTON))
+    {
+        user_button_millis = millisNow;
+        last_play_millis = millis();
+    }
+    user_ack = (millisNow - user_button_millis < BUTTON_PAIRING_WINDOW_MS) && user_button_millis>0;
+    standby = !(millisNow - last_play_millis < NO_SOUND_TIMEOUT_MS);
+
     if (user_ack)
     {
         static_led_color = 0;
-        blink_led_color = 0xffffff;
+        blink_led_color = 0xFDF8E2;
     }
 
     if (!user_ack && APP_STATE == APP_STA_DISCONNECTED)
@@ -211,10 +209,22 @@ void loop()
 
     if (APP_STATE == APP_STA_CONNECTED)
     {
-        static_led_color = 0xa0fff0f;
+        static_led_color = 0x0FFF0F;
+        blink_led_color = 0;
+    }
+
+    if (APP_STATE == APP_STA_PAIRING && !user_ack)
+    {
+        static_led_color = 0;
+        blink_led_color = 0xFDF8E2;
+    }
+
+    if (APP_STATE == APP_STA_CONNECTED && standby)
+    {
+        static_led_color = 0x0F0A00;
         blink_led_color = 0;
     }
 
     led_control();
-    delay(100);
+    delay(50);
 }
